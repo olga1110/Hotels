96a45e15e7a57c0d544322de8c570c3118a9c8a7
olga1110 <olga1110@github.com>
2025-03-23 23:54:44 +0500

    аутентификация/авторизация

--- src/config.py
+++ src/config.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file=f"{os.path.dirname(os.path.abspath(__file__))}/../.env")
 #     env_file = Path(__file__).parent.parent / ".env"
--- src/main.py
+++ src/main.py
@@ -19,4 +19,4 @@ app.include_router(router_hotels)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8920, reload=True)
