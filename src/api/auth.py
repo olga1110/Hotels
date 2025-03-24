e58d2e00a31f3b9ab69fa66069008d6cbedb64fc
olga1110 <olga1110@github.com>
2025-03-24 23:57:30 +0500

    get cookies

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.repositories.users import UsersRepository
@@ -36,3 +36,10 @@ async def login_user(
         access_token = AuthServices().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
     return {"access_token": access_token}
+
+
+@router.get('/only_auth')
+async def only_auth(
+        request: Request
+):
+    return request.cookies.get('access_token')
